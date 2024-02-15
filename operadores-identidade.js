9a4abff78304e86dc337247f02dcbbb6f7bcee33
isabeladomingos <domingosisabela@icloud.com>
2024-02-15 02:34:03 -0300

    condicionais

---
+++ operadores-identidade.js
@@ -0,0 +1,12 @@
+const naoIdentico = 18 === "18";
+console.log(naoIdentico);
+
+const verdade = 18 == "18";
+console.log(verdade);
+
+// === - mesmo conteúdo e mesmo tipo.
+
+const ehIdentico = 18 === 18;
+console.log(ehIdentico);
+
+//!=== não é identico
---
+++ operadores-igualdade.js
@@ -0,0 +1,5 @@
+const ehIgual = 18 == 18;
+console.log(ehIgual);
+
+const ehDiferente = 19 != 18;
+console.log(ehDiferente);
